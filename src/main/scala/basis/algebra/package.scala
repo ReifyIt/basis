54041793b50c09a205b37710e15e4900084ea4b0
Chris Sachs <c9r@me.com>
2012-03-24 12:19:52 -0700

    Added Matrix Spaces.

--- src/main/scala/basis/algebra/package.scala
+++ src/main/scala/basis/algebra/package.scala
@@ -45,6 +45,15 @@ package object algebra {
     type Scalar <: OrderedRing[Scalar] with CompleteField[Scalar]
   }
   
+  type MatrixSpace = Singleton {
+    type Matrix       <: basis.algebra.Matrix[Matrix, Transpose, ColumnVector, RowVector, Scalar]
+    type Transpose    <: basis.algebra.Matrix[Transpose, Matrix, RowVector, ColumnVector, Scalar]
+    type ColumnVector <: LinearVector[ColumnVector, Scalar]
+    type RowVector    <: LinearVector[RowVector, Scalar]
+    type Scalar       <: Field[Scalar]
+    type Vector       >: Matrix
+  }
+  
   type R2 = Singleton {
     type Point  = PointR2
     type Vector = VectorR2
@@ -55,6 +64,38 @@ package object algebra {
     type Point  = PointR3
     type Vector = VectorR3
     type Scalar = Real
+  }
+  
+  type R4 = Singleton {
+    type Vector = VectorR4
+    type Scalar = Real
+  }
+  
+  type R2x2 = Singleton {
+    type Matrix       = MatrixR2x2
+    type Transpose    = MatrixR2x2
+    type ColumnVector = VectorR2
+    type RowVector    = VectorR2
+    type Scalar       = Real
+    type Vector       = MatrixR2x2
+  }
+  
+  type R3x3 = Singleton {
+    type Matrix       = MatrixR3x3
+    type Transpose    = MatrixR3x3
+    type ColumnVector = VectorR3
+    type RowVector    = VectorR3
+    type Scalar       = Real
+    type Vector       = MatrixR3x3
+  }
+  
+  type R4x4 = Singleton {
+    type Matrix       = MatrixR4x4
+    type Transpose    = MatrixR4x4
+    type ColumnVector = VectorR4
+    type RowVector    = VectorR4
+    type Scalar       = Real
+    type Vector       = MatrixR4x4
   }
   
   implicit def DoubleToReal(value: Double): Real = new Real(value)
