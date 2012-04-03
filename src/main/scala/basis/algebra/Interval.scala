0b598ebfd8461aee288746aaccb3f3f720c4308e
Chris Sachs <c9r@me.com>
2012-04-03 14:54:15 -0700

    Refactor intervals.
    Rename IntervalZ1 to just plain Interval. I considered naming it
    DiscreteInterval but I don't plan to abstract over intervals so the
    simple name goes to discrete intervals. If I need an interval
    abstraction it can live with a longer name. I will call real intervals,
    wait for it, RealInterval.
    
    Intervals now handle the special values 'empty' and 'NaI' (Not an
    Interval). IntervalZ2 no longer inlines interval calculations.

---
+++ src/main/scala/basis/algebra/Interval.scala
@@ -0,0 +1,195 @@
+/*      ____              ___                                           *\
+**     / __ | ___  ____  /__/___      A library of building blocks      **
+**    / __  / __ |/ ___|/  / ___|                                       **
+**   / /_/ / /_/ /\__ \/  /\__ \      (c) 2012 Chris Sachs              **
+**  |_____/\_____\____/__/\____/      http://www.scalabasis.com/        **
+\*                                                                      */
+
+package basis.algebra
+
+import scala.math.{min, max}
+
+import basis.memory._
+import basis.util.MurmurHash._
+
+/** A discrete closed interval implementing arithmetic operations.
+  * 
+  * @author Chris Sachs
+  * 
+  * @constructor Constructs an interval with a lower and upper bound.
+  * @param  lower   The lower bound of this $interval.
+  * @param  upper   The upper bound of this $interval.
+  * 
+  * @define Element   $Interval
+  * @define element   $interval
+  * @define Interval  Interval
+  * @define interval  discrete interval
+  */
+final class Interval private (val lower: Long, val upper: Long) extends Ring[Interval] {
+  /** Returns `true` if this is '''N'''ot '''a'''n '''I'''nterval. */
+  def isNaI: Boolean = lower > upper && lower > 0L
+  
+  /** Returns `true` if this $interval contains no values. */
+  def isEmpty: Boolean = lower > upper
+  
+  /** Returns the number of values enclosed by this $interval. */
+  def size: Long = upper - lower + 1L
+  
+  /** Returns `true` if this $interval encloses the given value. */
+  def contains(i: Long): Boolean = lower <= i && i <= upper
+  
+  def + (that: Interval): Interval = {
+    if (that.isEmpty) { if (isNaI) this else that } else if (isEmpty) this
+    else new Interval(lower + that.lower, upper + that.upper)
+  }
+  
+  def + (n: Int): Interval = {
+    if (isEmpty) this
+    else new Interval(lower + n, upper + n)
+  }
+  
+  def unary_- : Interval = {
+    if (isEmpty) this
+    else new Interval(-upper, -lower)
+  }
+  
+  def - (that: Interval): Interval = {
+    if (that.isEmpty) { if (isNaI) this else that } else if (isEmpty) this
+    else new Interval(lower - that.upper, upper - that.lower)
+  }
+  
+  def - (n: Int): Interval = {
+    if (isEmpty) this
+    else new Interval(lower - n, upper - n)
+  }
+  
+  def * (that: Interval): Interval = {
+    if (that.isEmpty) { if (isNaI) this else that } else if (isEmpty) this
+    else {
+      val ll = lower * that.lower
+      val lu = lower * that.upper
+      val ul = upper * that.lower
+      val uu = upper * that.upper
+      new Interval(min(min(min(ll, lu), ul), uu), max(max(max(ll, lu), ul), uu))
+    }
+  }
+  
+  def * (n: Int): Interval = {
+    if (isEmpty) this
+    else if (n >= 0) new Interval(lower * n, upper * n)
+    else new Interval(upper * n, lower * n)
+  }
+  
+  def pow(n: Int): Interval = {
+    require(n >= 0, "negative exponent")
+    if (isEmpty) this
+    else {
+      val lowerN = math.pow(lower, n).toLong
+      val upperN = math.pow(upper, n).toLong
+      if ((n & 1) != 0 || lower >= 0L) // odd exponent or positive interval
+        new Interval(lowerN, upperN)
+      else if (upper <  0L) // negative interval
+        new Interval(upperN, lowerN)
+      else // interval spans zero
+        new Interval(0L, max(lowerN, upperN))
+    }
+  }
+  
+  /** Returns the intersection of this $interval and another $interval.
+    * 
+    * @param  that  the $interval to intersect with.
+    * @return the intersecting interval or else the empty interval.
+    */
+  def intersect(that: Interval): Interval = {
+    if (that.isEmpty) { if (isNaI) this else that } else if (isEmpty) this
+    else {
+      val greatestLower = max(lower, that.lower)
+      val leastUpper = min(upper, that.upper)
+      if (greatestLower <= leastUpper) new Interval(greatestLower, leastUpper)
+      else Interval.empty
+    }
+  }
+  
+  /** Applies a function to each value contained by this $interval. */
+  @inline final def foreach[@specialized(Unit) U](f: Long => U) {
+    var i = lower
+    val upper = this.upper
+    while (i <= upper) {
+      f(i)
+      i += 1L
+    }
+  }
+  
+  override def equals(other: Any): Boolean = other match {
+    case that: Interval => lower == that.lower && upper == that.upper
+    case _ => false
+  }
+  
+  override def hashCode: Int =
+    mash(mix(mix(1851844763, lower), upper))
+  
+  override def toString: String =
+    if (isNaI) "[NaI]" else if (isEmpty) "[]" else "["+ lower +", "+ upper +"]"
+}
+
+/** Contains factory methods and implicit conversions for discrete intervals. */
+object Interval {
+  /** The '''N'''ot '''a'''n '''I'''nterval value. */
+  val NaI: Interval = new Interval(1L, 0L)
+  
+  /** The empty interval. */
+  val empty: Interval = new Interval(0L, -1L)
+  
+  /** Creates a new interval with a lower and upper bound.
+    * 
+    * @param  lower   the lower bound of the interval.
+    * @param  upper   the upper bound of the interval.
+    * @return the interval [`lower`, `upper`] or `NaI` if `lower` > `upper`.
+    */
+  def apply(lower: Long, upper: Long): Interval =
+    if (lower <= upper) new Interval(lower, upper) else NaI
+  
+  def unapply(interval: Interval): Option[(Long, Long)] =
+    if (interval.isEmpty) None else Some(interval.lower, interval.upper)
+  
+  /** Returns an improper interval containing just the given value. */
+  implicit def improper(value: Long): Interval = new Interval(value, value)
+  
+  /** The discrete interval additive identity. */
+  implicit val additiveIdentity = new AdditiveIdentity(improper(0L))
+  
+  /** The discrete interval multiplicative identity. */
+  implicit val multiplicativeIdentity = new MultiplicativeIdentity(improper(1L))
+  
+  /** The default struct for discrete intervals. */
+  implicit lazy val struct = new StructInterval
+  
+  /** A struct for discrete intervals. */
+  class StructInterval(frameOffset: Long, frameSize: Long, frameAlignment: Long)
+    extends Struct2[Long, Long, Interval](frameOffset, frameSize, frameAlignment) {
+    
+    def this() = this(0L, 0L, 0L)
+    
+    /** The `lower` bound field projection of this struct. */
+    def lower: Struct[Long] = field1
+    
+    /** The `upper` bound field projection of this struct. */
+    def upper: Struct[Long] = field2
+    
+    def load(data: Data, address: Long): Interval = {
+      val lower = data.loadLong(address + offset1)
+      val upper = data.loadLong(address + offset2)
+      new Interval(lower, upper)
+    }
+    
+    def store(data: Data, address: Long, interval: Interval) {
+      data.storeLong(address + offset1, interval.lower)
+      data.storeLong(address + offset2, interval.upper)
+    }
+    
+    override def project(offset: Long, size: Long, alignment: Long): StructInterval =
+      new StructInterval(offset1 + offset, size, alignment)
+    
+    override def toString: String = "StructInterval"
+  }
+}
--- src/main/scala/basis/algebra/IntervalZ2.scala
+++ src/main/scala/basis/algebra/IntervalZ2.scala
@@ -12,45 +12,22 @@ import scala.math.{min, max}
 import basis.memory._
 import basis.util.MurmurHash._
 
-/** A 2-dimensional vector of discrete closed intervals.
+/** A 2-dimensional vector of discrete intervals.
   * 
   * @author Chris Sachs
   * 
-  * @constructor Constructs an interval vector with bounds for each axis.
-  * @param  lowerX  The lower bound on the ''x''-axis.
-  * @param  upperX  The upper bound on the ''x''-axis.
-  * @param  lowerY  The lower bound on the ''y''-axis.
-  * @param  upperY  The upper bound on the ''y''-axis.
+  * @constructor Constructs a 2D interval with two coordinate bounds.
+  * @param  x   The ''x''-axis bounds.
+  * @param  y   The ''y''-axis bounds.
   * 
-  * @define vector  discrete 2D interval
+  * @define vector  2D interval vector
   * @define scalar  discrete interval
   */
-final class IntervalZ2(
-    val lowerX: Long, val upperX: Long,
-    val lowerY: Long, val upperY: Long)
-  extends Vector[IntervalZ2, IntervalZ1] {
-  
-  checkBounds()
-  private def checkBounds() {
-    require(lowerX <= upperX, "lower x-bound greater than upper x-bound")
-    require(lowerY <= upperY, "lower y-bound greater than upper y-bound")
-  }
-  
-  def this(x: IntervalZ1, y: IntervalZ1) =
-    this(x.lower, x.upper, y.lower, y.upper)
-  
-  /** The ''x''-axis bounds of this $vector. */
-  def x: IntervalZ1 = new IntervalZ1(lowerX, upperX)
-  
-  /** The ''y''-axis bounds of this $vector. */
-  def y: IntervalZ1 = new IntervalZ1(lowerY, upperY)
-  
-  private def x_size: Long = upperX - lowerX + 1L
-  
-  private def y_size: Long = upperY - lowerY + 1L
+final class IntervalZ2(val x: Interval, val y: Interval)
+  extends Vector[IntervalZ2, Interval] {
   
   /** Returns the number of values enclosed by this $vector. */
-  def size: Long = x_size * y_size
+  def size: Long = x.size * y.size
   
   /** Returns `true` if this $vector encloses the given coordinates.
     * 
@@ -58,51 +35,41 @@ final class IntervalZ2(
     * @param  j   the ''y''-coordinate; tested against the ''y''-axis bounds.
     * @return `true` if both coordinates are bounded.
     */
-  def contains(i: Long, j: Long): Boolean =
-    lowerX <= i && i <= upperX && lowerY <= j && j <= upperY
+  def contains(i: Long, j: Long): Boolean = x.contains(i) && y.contains(j)
   
   def + (that: IntervalZ2): IntervalZ2 =
-    new IntervalZ2(
-      lowerX + that.lowerX, upperX + that.upperX,
-      lowerY + that.lowerY, upperY + that.upperY)
+    new IntervalZ2(x + that.x, y + that.y)
   
   def unary_- : IntervalZ2 =
-    new IntervalZ2(-upperX, -lowerX, -upperY, -lowerY)
+    new IntervalZ2(-x, -y)
   
   def - (that: IntervalZ2): IntervalZ2 =
-    new IntervalZ2(
-      lowerX - that.upperX, upperX - that.lowerX,
-      lowerY - that.upperY, upperY - that.lowerY)
+    new IntervalZ2(x - that.x, y - that.y)
   
-  def :* (scalar: IntervalZ1): IntervalZ2 = {
-    val llx = lowerX * scalar.lower
-    val lux = lowerX * scalar.upper
-    val ulx = upperX * scalar.lower
-    val uux = upperX * scalar.upper
-    val lly = lowerY * scalar.lower
-    val luy = lowerY * scalar.upper
-    val uly = upperY * scalar.lower
-    val uuy = upperY * scalar.upper
-    new IntervalZ2(
-      min(min(min(llx, lux), ulx), uux),
-      max(max(max(llx, lux), ulx), uux),
-      min(min(min(lly, luy), uly), uuy),
-      max(max(max(lly, luy), uly), uuy))
-  }
+  def :* (scalar: Interval): IntervalZ2 =
+    new IntervalZ2(x * scalar, y * scalar)
   
-  def *: (scalar: IntervalZ1): IntervalZ2 = this :* scalar
+  def *: (scalar: Interval): IntervalZ2 = this :* scalar
+  
+  /** Returns the intersection of this $vector and another $vector.
+    * 
+    * @param  that  the $vector to intersect with.
+    * @return the vector intersection of each axis.
+    */
+  def intersect(that: IntervalZ2): IntervalZ2 =
+    new IntervalZ2(x intersect that.x, y intersect that.y)
   
   /** Applies a function to each coordinate pair contained by this $vector. */
   @inline final def foreach[@specialized(Unit) U](f: (Long, Long) => U) {
     var i = 0L // reserve jvm local variable
     var j = 0L // reserve jvm local variable
-    val upperX = this.upperX
-    val lowerX = this.lowerX
-    val upperY = this.upperY
-    j = lowerY
-    while (j <= upperY) {
-      i = lowerX
-      while (i <= upperX) {
+    val x_upper = x.upper
+    val x_lower = x.lower
+    val y_upper = y.upper
+    j = y.lower
+    while (j <= y_upper) {
+      i = x_lower
+      while (i <= x_upper) {
         f(i, j)
         i += 1L
       }
@@ -111,82 +78,58 @@ final class IntervalZ2(
   }
   
   override def equals(other: Any): Boolean = other match {
-    case that: IntervalZ2 =>
-      lowerX == that.lowerX && upperX == that.upperX &&
-      lowerY == that.lowerY && upperY == that.upperY
+    case that: IntervalZ2 => x.equals(that.x) && y.equals(that.y)
     case _ => false
   }
   
   override def hashCode: Int =
-    mash(mix(mix(mix(mix(1506359539, lowerX), upperX), lowerY), upperY))
+    mash(mix(mix(1506359539, x), y))
   
   override def toString: String =
     "IntervalZ2"+"("+ x +", "+ y +")"
 }
 
-/** Contains factory methods for discrete 2D intervals. */
+/** Contains factory methods for 2D interval vectors. */
 object IntervalZ2 {
-  def apply(lowerX: Long, upperX: Long, lowerY: Long, upperY: Long): IntervalZ2 =
-    new IntervalZ2(lowerX, upperX, lowerY, upperY)
-  
-  def apply(x: IntervalZ1, y: IntervalZ1): IntervalZ2 =
+  def apply(x: Interval, y: Interval): IntervalZ2 =
     new IntervalZ2(x, y)
   
-  def unapply(interval: IntervalZ2): Some[(IntervalZ1, IntervalZ1)] =
-    Some(interval.x, interval.y)
+  def unapply(vector: IntervalZ2): Some[(Interval, Interval)] =
+    Some(vector.x, vector.y)
   
-  /** Returns a degenerate discrete 2D interval containing just the given coordinates. */
-  def degenerate(x: Long, y: Long): IntervalZ2 = new IntervalZ2(x, x, y, y)
+  /** The 2D interval vector additive identity. */
+  implicit val additiveIdentity = new AdditiveIdentity(new IntervalZ2(Zero, Zero))
   
-  /** The discrete 2D interval additive identity. */
-  implicit val additiveIdentity = new AdditiveIdentity(degenerate(0L, 0L))
-  
-  /** The default struct for discrete 2D intervals. */
+  /** The default struct for 2D interval vectors. */
   implicit lazy val struct = new StructIntervalZ2
   
-  /** A struct for discrete 2D intervals. */
+  /** A struct for 2D interval vectors. */
   class StructIntervalZ2(frameOffset: Long, frameSize: Long, frameAlignment: Long)
-    extends Struct2[IntervalZ1, IntervalZ1, IntervalZ2](frameOffset, frameSize, frameAlignment) {
+    extends Struct2[Interval, Interval, IntervalZ2](frameOffset, frameSize, frameAlignment) {
     
-    import IntervalZ1.StructIntervalZ1
+    import Interval.StructInterval
     
     def this() = this(0L, 0L, 0L)
     
-    protected override val field1 = new StructIntervalZ1(offset1, size, alignment)
+    protected override val field1 = new StructInterval(offset1, size, alignment)
     
-    protected override val field2 = new StructIntervalZ1(offset2, size, alignment)
+    protected override val field2 = new StructInterval(offset2, size, alignment)
     
-    /** The `x` field projection of this struct. */
-    def x: StructIntervalZ1 = field1
+    /** The `x`-interval field projection of this struct. */
+    def x: StructInterval = field1
     
-    /** The `y` field projection of this struct. */
-    def y: StructIntervalZ1 = field2
-    
-    /** The `lowerX` field projection of this struct. */
-    def lowerX: Struct[Long] = x.lower
-    
-    /** The `upperX` field projection of this struct. */
-    def upperX: Struct[Long] = x.upper
-    
-    /** The `lowerY` field projection of this struct. */
-    def lowerY: Struct[Long] = y.lower
-    
-    /** The `upperY` field projection of this struct. */
-    def upperY: Struct[Long] = y.upper
+    /** The `y`-interval field projection of this struct. */
+    def y: StructInterval = field2
     
     def load(data: Data, address: Long): IntervalZ2 = {
-      val lowerX = field1.lower.load(data, address)
-      val upperX = field1.upper.load(data, address)
-      val lowerY = field2.lower.load(data, address)
-      val upperY = field2.upper.load(data, address)
-      new IntervalZ2(lowerX, upperX, lowerY, upperY)
+      val x = field1.load(data, address)
+      val y = field2.load(data, address)
+      new IntervalZ2(x, y)
     }
     
-    def store(data: Data, address: Long, interval: IntervalZ2) {
-      field1.lower.store(data, address, interval.lowerX)
-      field1.upper.store(data, address, interval.upperX)
-      field2.lower.store(data, address, interval.lowerY)
-      field2.upper.store(data, address, interval.upperY)
+    def store(data: Data, address: Long, vector: IntervalZ2) {
+      field1.store(data, address, vector.x)
+      field2.store(data, address, vector.y)
     }
     
     override def project(offset: Long, size: Long, alignment: Long): StructIntervalZ2 =
