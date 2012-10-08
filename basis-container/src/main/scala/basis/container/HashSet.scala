380f819a410b943b9815655ba1c979a2e1eda075
Chris Sachs <c9r@me.com>
2012-10-07 18:06:51 -0700

    Handle item removal from collision sets.

--- basis-container/src/main/scala/basis/container/HashSet.scala
+++ basis-container/src/main/scala/basis/container/HashSet.scala
@@ -39,12 +39,9 @@ final class HashSet[A] private (
         slots(i).asInstanceOf[HashSet[A]].contains(item, hash >>> 5)
       else false
     }
-    else { // empty set or collision array
-      var i = 0
-      while (i < slots.length) { // linearly search array for item
+    else { // search collision array
+      for (i <- 0 until slots.length) // linearly search array for item
         if (slots(i).equals(item)) return true;
-        i += 1
-      }
       false
     }
   }
@@ -59,8 +56,8 @@ final class HashSet[A] private (
     else if (nodeMap != 0) { // test for non-empty hash trie
       val i = (nodeMap & (n - 1)).bitCount // take hamming weight of n-bit node map as array index
       if ((nodeMap & n) != 0) { // test if slot already contains a node
-        val node = slots(i)
         if ((itemMap & n) != 0) { // test if slot contains an item
+          val node = slots(i)
           if (node.equals(item)) this // return unmodified sets
           else { // merge new item with existing item
             val newNode = resolve(item.asInstanceOf[AnyRef], hash >>> (shift + 5), node, node.hashCode >>> (shift + 5))
@@ -70,7 +67,8 @@ final class HashSet[A] private (
           }
         }
         else { // update existing subset with new item
-          val newNode = node.asInstanceOf[HashSet[A]].updated(item, hash, shift + 5)
+          val node = slots(i).asInstanceOf[HashSet[A]]
+          val newNode = node.updated(item, hash, shift + 5)
           if (node eq newNode) this // return unmodified sets
           else { // replace existing subset with updated subset
             val newSlots = slots.clone
@@ -134,41 +132,54 @@ final class HashSet[A] private (
   }
   
   private def removed(item: A, hash: Int): HashSet[A] = {
-    val n = 1 << (hash & 0x1F) // convert low 5 hash bits to an index bit
-    if ((nodeMap & n) != 0) { // test if slot contains a node
-      val i = (nodeMap & (n - 1)).bitCount // take hamming weight of n-bit node map as array index
-      if ((itemMap & n) != 0) { // test if slot contains an item
-        val node = slots(i)
-        if (node.equals(item)) { // remove item from trie
-          val newSlots = new scala.Array[AnyRef](slots.length - 1)
-          java.lang.System.arraycopy(slots, 0, newSlots, 0, i)
-          java.lang.System.arraycopy(slots, i + 1, newSlots, i, newSlots.length - i)
-          new HashSet[A](nodeMap & ~n, itemMap & ~n, newSlots)
+    if (nodeMap != 0) { // test for non-empty hash trie
+      val n = 1 << (hash & 0x1F) // convert low 5 hash bits to an index bit
+      if ((nodeMap & n) != 0) { // test if slot contains a node
+        val i = (nodeMap & (n - 1)).bitCount // take hamming weight of n-bit node map as array index
+        if ((itemMap & n) != 0) { // test if slot contains an item
+          val node = slots(i)
+          if (node.equals(item)) { // remove item from trie
+            val newSlots = new scala.Array[AnyRef](slots.length - 1)
+            java.lang.System.arraycopy(slots, 0, newSlots, 0, i)
+            java.lang.System.arraycopy(slots, i + 1, newSlots, i, newSlots.length - i)
+            new HashSet[A](nodeMap & ~n, itemMap & ~n, newSlots)
+          }
+          else this // item not in set
         }
-        else this // item not in set
+        else { // slot contains a subset
+          val node = slots(i).asInstanceOf[HashSet[A]]
+          val newNode = node.removed(item, hash >>> 5) // remove item from subset
+          if (node eq newNode) this // return unmodified sets
+          else if (node.slots.length == 0) { // remove empty subsets
+            val newSlots = new scala.Array[AnyRef](slots.length - 1)
+            java.lang.System.arraycopy(slots, 0, newSlots, 0, i)
+            java.lang.System.arraycopy(slots, i + 1, newSlots, i, newSlots.length - i)
+            new HashSet[A](nodeMap ^ n, itemMap, newSlots)
+          }
+          else if (node.slots.length == 1) { // lift unary subsets
+            val newSlots = slots.clone
+            newSlots(i) = newNode.slots(0)
+            new HashSet[A](nodeMap, itemMap | n, newSlots)
+          }
+          else { // replace existing subset with updated subset
+            val newSlots = slots.clone
+            newSlots(i) = newNode
+            new HashSet[A](nodeMap, itemMap, newSlots)
+          }
+        }
       }
-      else { // slot contains a subset
-        val node = slots(i).asInstanceOf[HashSet[A]]
-        val newNode = node.removed(item, hash >>> 5) // remove item from subset
-        if (node eq newNode) this // return unmodified sets
-        else if (node.nodeMap == 0) { // remove empty subsets
-          val newSlots = new scala.Array[AnyRef](slots.length - 1)
-          java.lang.System.arraycopy(slots, 0, newSlots, 0, i)
-          java.lang.System.arraycopy(slots, i + 1, newSlots, i, newSlots.length - i)
-          new HashSet[A](nodeMap ^ n, itemMap, newSlots)
-        }
-        else if (node.nodeMap.bitCount == 1) { // lift unary subsets
-          val newSlots = slots.clone
-          newSlots(i) = newNode.slots(0)
-          new HashSet[A](nodeMap, itemMap | n, newSlots)
-        }
-        else { // replace existing subset with updated subset
-          val newSlots = slots.clone
-          newSlots(i) = newNode
-          new HashSet[A](nodeMap, itemMap, newSlots)
-        }
-      }
+      else this // item not in set
     }
-    else this // return unmodified sets
+    else { // search collision array
+      for (i <- 0 until slots.length) { // linearly search array for item
+        if (slots(i).equals(item)) { // remove found item from collision array
+          val newSlots = new scala.Array[AnyRef](slots.length - 1)
+          java.lang.System.arraycopy(slots, 0, newSlots, 0, i)
+          java.lang.System.arraycopy(slots, i + 1, newSlots, i, newSlots.length - i)
+          return new HashSet[A](0, 0, newSlots)
+        }
+      }
+      this // item not in set
+    }
   }
 }
