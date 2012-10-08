56802a582b48c9a5dc7b778d4527e36a0a62922f
Chris Sachs <c9r@me.com>
2012-10-08 16:17:50 -0700

    Add Equal and Hashable typeclasses.

---
+++ basis/src/main/scala/basis/Equal.scala
@@ -0,0 +1,12 @@
+/*      ____              ___                                           *\
+**     / __ | ___  ____  /__/___      A library of building blocks      **
+**    / __  / __ |/ ___|/  / ___|                                       **
+**   / /_/ / /_/ /\__ \/  /\__ \      (c) 2012 Chris Sachs              **
+**  |_____/\_____\____/__/\____/      http://www.scalabasis.com/        **
+\*                                                                      */
+
+package basis
+
+trait Equal[-A] {
+  def equal(x: A, y: A): Boolean
+}
---
+++ basis/src/main/scala/basis/Hashable.scala
@@ -0,0 +1,12 @@
+/*      ____              ___                                           *\
+**     / __ | ___  ____  /__/___      A library of building blocks      **
+**    / __  / __ |/ ___|/  / ___|                                       **
+**   / /_/ / /_/ /\__ \/  /\__ \      (c) 2012 Chris Sachs              **
+**  |_____/\_____\____/__/\____/      http://www.scalabasis.com/        **
+\*                                                                      */
+
+package basis
+
+trait Hashable[-A] extends Equal[A] {
+  def hash(x: A): Int
+}
