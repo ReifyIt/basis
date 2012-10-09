50caad4af641af6ebad6a690efbce555d55abe95
Chris Sachs <c9r@me.com>
2012-10-09 15:06:35 -0700

    Make sbt console work despite -Yno-imports.

--- project/BasisBuild.scala
+++ project/BasisBuild.scala
@@ -14,7 +14,14 @@ object BasisBuild extends Build {
   lazy val all = Project(
     id           = "all",
     base         = file("."),
-    settings     = commonSettings,
+    settings     = basicSettings,
+    dependencies = Seq(
+      Basis,
+      BasisCollection,
+      BasisContainer,
+      BasisData,
+      BasisText,
+      BasisUtil),
     aggregate    = Seq(
       Basis,
       BasisCollection,
@@ -33,74 +40,71 @@ object BasisBuild extends Build {
   lazy val BasisCollection = Project(
     id           = "basis-collection",
     base         = file("basis-collection"),
-    settings     = commonSettings ++ Seq(
-      libraryDependencies += "org.scala-lang" % "scala-reflect" % "2.11.0-SNAPSHOT" % "provided"
-    ),
+    settings     = commonSettings,
     dependencies = Seq(Basis)
   )
   
   lazy val BasisContainer = Project(
     id           = "basis-container",
     base         = file("basis-container"),
-    settings     = commonSettings ++ Seq(
-      libraryDependencies += "org.scala-lang" % "scala-reflect" % "2.11.0-SNAPSHOT" % "provided"
-    ),
+    settings     = commonSettings,
     dependencies = Seq(Basis, BasisData)
   )
   
   lazy val BasisData = Project(
     id           = "basis-data",
     base         = file("basis-data"),
-    settings     = commonSettings ++ Seq(
-      libraryDependencies += "org.scala-lang" % "scala-reflect" % "2.11.0-SNAPSHOT" % "provided"
-    )
+    settings     = commonSettings
   )
   
   lazy val BasisText = Project(
     id           = "basis-text",
     base         = file("basis-text"),
-    settings     = commonSettings ++ Seq(
-      libraryDependencies += "org.scala-lang" % "scala-reflect" % "2.11.0-SNAPSHOT" % "provided"
-    ),
+    settings     = commonSettings,
     dependencies = Seq(Basis)
   )
   
   lazy val BasisUtil = Project(
     id           = "basis-util",
     base         = file("basis-util"),
-    settings     = commonSettings ++ Seq(
-      libraryDependencies += "org.scala-lang" % "scala-reflect" % "2.11.0-SNAPSHOT" % "provided"
-    ),
+    settings     = commonSettings,
     dependencies = Seq(Basis)
   )
   
   lazy val commonSettings =
+    basicSettings   ++
+    compileSettings ++
+    publishSettings
+  
+  lazy val basicSettings =
     Defaults.defaultSettings ++
     Unidoc.settings          ++
     projectSettings          ++
-    compileSettings          ++
-    publishSettings
+    docSettings
   
   lazy val projectSettings = Seq(
+    scalaVersion := "2.11.0-SNAPSHOT",
     version      := "0.0-SNAPSHOT",
     organization := "com.scalabasis",
     description  := "A library of building blocks",
     homepage     := Some(url("http://www.scalabasis.com/")),
-    licenses     := Seq("MIT" -> url("http://www.opensource.org/licenses/mit-license.php"))
+    licenses     := Seq("MIT" -> url("http://www.opensource.org/licenses/mit-license.php")),
+    resolvers    += Resolver.sonatypeRepo("snapshots")
   )
   
   lazy val compileSettings = Seq(
-    scalaVersion := "2.11.0-SNAPSHOT",
     scalacOptions in Compile ++= Seq("-optimise", "-Xno-forwarders", "-Yno-imports", "-Yinline-warnings", "-Ywarn-all"),
-    scalacOptions in doc <++= (version, baseDirectory in LocalProject("basis")) map {
-      (version, baseDirectory) =>
-        val tagOrBranch = if (version.endsWith("-SNAPSHOT")) "master" else "v" + version
-        val docSourceUrl = "https://github.com/scalabasis/basis/tree/" + tagOrBranch + "€{FILE_PATH}.scala"
-        Seq("-Yno-imports", "-implicits", "-diagrams", "-sourcepath",
-            baseDirectory.getAbsolutePath, "-doc-source-url", docSourceUrl)
-    },
-    resolvers += Resolver.sonatypeRepo("snapshots")
-    //libraryDependencies += "org.scalatest" % "scalatest_2.10.0-M7" % "1.8-SNAPSHOT" % "test"
+    libraryDependencies += "org.scala-lang" % "scala-reflect" % "2.11.0-SNAPSHOT" % "provided"
+    //libraryDependencies += "org.scalatest" % "scalatest_2.10.0-SNAPSHOT" % "1.8-SNAPSHOT" % "test"
+  )
+  
+  lazy val docSettings = Seq(
+    scalacOptions in doc <++= (version, baseDirectory in LocalProject("basis")) map { (version, baseDirectory) =>
+      val tagOrBranch = if (version.endsWith("-SNAPSHOT")) "master" else "v" + version
+      val docSourceUrl = "https://github.com/scalabasis/basis/tree/" + tagOrBranch + "€{FILE_PATH}.scala"
+      Seq("-Yno-imports", "-implicits", "-diagrams", "-sourcepath",
+          baseDirectory.getAbsolutePath, "-doc-source-url", docSourceUrl)
+    }
   )
   
   lazy val publishSettings = Seq(
