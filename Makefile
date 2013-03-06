d20a72a068798f46ed44734eb99b475c3a2f0002
Chris Sachs <chris@reify.it>
2013-03-05 16:14:24 -0800

    Fix Makefile indentation

--- Makefile
+++ Makefile
@@ -1,12 +1,12 @@
 export BASEDIR=$(CURDIR)
 
 export BASIS_MEMORY_OBJECTS = \
-  basis/memory/JNILibrary.o \
-  basis/memory/package.o \
-  basis/memory/NativeData.o
+	basis/memory/JNILibrary.o \
+	basis/memory/package.o \
+	basis/memory/NativeData.o
 
 init:
-  $(MAKE) -f project/macosx-universal.mk init
+	$(MAKE) -f project/macosx-universal.mk init
 
 macosx-universal:
-  $(MAKE) -f project/macosx-universal.mk
+	$(MAKE) -f project/macosx-universal.mk
--- project/macosx-universal.mk
+++ project/macosx-universal.mk
@@ -7,11 +7,11 @@ LDFLAGS += -dynamiclib -framework JavaVM -arch x86_64 -arch i386
 all: $(BASEDIR)/basis-memory/target/libbasis-memory.macosx-universal.jnilib
 
 init:
-  mkdir -p $(BASEDIR)/basis-memory/target/macosx-universal/
-  mkdir -p $(BASEDIR)/basis-memory/target/macosx-universal/basis/memory
+	mkdir -p $(BASEDIR)/basis-memory/target/macosx-universal/
+	mkdir -p $(BASEDIR)/basis-memory/target/macosx-universal/basis/memory
 
 $(BASEDIR)/basis-memory/target/macosx-universal/%.o : $(BASEDIR)/basis-memory/src/main/native/%.c
-  $(CC) $(CFLAGS) -I$(BASEDIR)/basis-memory/src/main/native -c $< -o $@
+	$(CC) $(CFLAGS) -I$(BASEDIR)/basis-memory/src/main/native -c $< -o $@
 
 $(BASEDIR)/basis-memory/target/libbasis-memory.macosx-universal.jnilib: $(addprefix $(BASEDIR)/basis-memory/target/macosx-universal/, $(BASIS_MEMORY_OBJECTS))
-  $(CC) $(LDFLAGS) -o $@ $^
+	$(CC) $(LDFLAGS) -o $@ $^
