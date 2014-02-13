caa69ac7ee201f4b989d4dd657f39693b4b844d2
Paul Phillips <paulp@improving.org>
2014-02-13 12:08:28 -0800

    Cheat my way around implicit ambiguity issue.
    The implicit ambiguity issue only arises when values are involved,
    so turn the relevant implicits into methods. That loses the toString
    methods for the time being.

--- form/src/main/scala/basis/form/Variant.scala
+++ form/src/main/scala/basis/form/Variant.scala
@@ -20,61 +20,17 @@ trait Variant
   with DateForm
   with BooleanForm
   with NullForm
-  with UndefinedForm { variant =>
+  with UndefinedForm {
 
-  import scala.runtime._
+  implicit def ObjectFormBuilder(): Builder[(String, AnyForm)] with From[ObjectForm] with State[ObjectForm] = ObjectForm.Builder()
+  implicit def SeqFormBuilder(): Builder[AnyForm] with From[SeqForm] with State[SeqForm]                    = SeqForm.Builder()
+  implicit def SetFormBuilder(): Builder[SetForm] with From[SetForm] with State[SetForm]                    = SetForm.Builder()
+  implicit def StringFormBuilder(): StringBuilder with From[StringForm] with State[StringForm]              = StringForm.Builder()
 
-  implicit def ObjectFormBuilder(): Builder[(String, AnyForm)] with From[ObjectForm] with State[ObjectForm] =
-    ObjectForm.Builder()
-
-  implicit def SeqFormBuilder(): Builder[AnyForm] with From[SeqForm] with State[SeqForm] =
-    SeqForm.Builder()
-
-  implicit def SetFormBuilder(): Builder[SetForm] with From[SetForm] with State[SetForm] =
-    SetForm.Builder()
-
-  implicit def StringFormBuilder(): StringBuilder with From[StringForm] with State[StringForm] =
-    StringForm.Builder()
-
-  implicit lazy val StringToForm: String => StringForm = new StringToForm
-
-  implicit lazy val IntToForm: Int => NumberForm = new IntToForm
-
-  implicit lazy val LongToForm: Long => NumberForm = new LongToForm
-
-  implicit lazy val FloatToForm: Float => NumberForm = new FloatToForm
-
-  implicit lazy val DoubleToForm: Double => NumberForm = new DoubleToForm
-
-  implicit lazy val BooleanToForm: Boolean => BooleanForm = new BooleanToForm
-
-  private final class StringToForm extends AbstractFunction1[String, StringForm] {
-    override def apply(value: String): StringForm = StringForm(value)
-    override def toString: String = "StringToForm"
-  }
-
-  private final class IntToForm extends AbstractFunction1[Int, NumberForm] {
-    override def apply(value: Int): NumberForm = NumberForm(value)
-    override def toString: String = "IntToForm"
-  }
-
-  private final class LongToForm extends AbstractFunction1[Long, NumberForm] {
-    override def apply(value: Long): NumberForm = NumberForm(value)
-    override def toString: String = "LongToForm"
-  }
-
-  private final class FloatToForm extends AbstractFunction1[Float, NumberForm] {
-    override def apply(value: Float): NumberForm = NumberForm(value)
-    override def toString: String = "FloatToForm"
-  }
-
-  private final class DoubleToForm extends AbstractFunction1[Double, NumberForm] {
-    override def apply(value: Double): NumberForm = NumberForm(value)
-    override def toString: String = "DoubleToForm"
-  }
-
-  private final class BooleanToForm extends AbstractFunction1[Boolean, BooleanForm] {
-    override def apply(value: Boolean): BooleanForm = if (value) TrueForm else FalseForm
-    override def toString: String = "BooleanToForm"
-  }
+  implicit def StringToForm(value: String): StringForm    = StringForm(value)
+  implicit def IntToForm(value: Int): NumberForm          = NumberForm(value)
+  implicit def LongToForm(value: Long): NumberForm        = NumberForm(value)
+  implicit def FloatToForm(value: Float): NumberForm      = NumberForm(value)
+  implicit def DoubleToForm(value: Double): NumberForm    = NumberForm(value)
+  implicit def BooleanToForm(value: Boolean): BooleanForm = if (value) TrueForm else FalseForm
 }
