8172bb982280ec3639d80165b7b13c02886bfafa
Chris Sachs <chris@reify.it>
2014-06-23 10:28:07 -0700

    Parameterize Protobuf.Unknown with default value

--- proto/src/main/scala/basis/proto/Protobuf.scala
+++ proto/src/main/scala/basis/proto/Protobuf.scala
@@ -93,8 +93,10 @@ object Protobuf {
   def Required[@specialized(Protobuf.Specialized) T](tag: Int)(implicit T: Protobuf[T]): Field[T]             = new Required(tag)(T)
   def Optional[@specialized(Protobuf.Specialized) T](tag: Int, default: T)(implicit T: Protobuf[T]): Field[T] = new Optional(tag, default)(T)
 
-  def Unknown(key: Long): Field[Unit] = new Unknown(key)
-  def Unknown(tag: Int, wireType: Int): Field[Unit] = new Unknown((tag.toLong << 3) | (wireType & 0x7).toLong)
+  def Unknown[T](key: Long, default: T)(implicit T: Protobuf[T]): Field[T] = new Unknown[T](key, default)
+  def Unknown[T](tag: Int, wireType: Int, default: T)(implicit T: Protobuf[T]): Field[T] = new Unknown[T](tag, wireType, default)
+  def Unknown(key: Long): Field[Unit] = Unknown(key, ())(Unit)
+  def Unknown(tag: Int, wireType: Int): Field[Unit] = Unknown(tag, wireType, ())(Unit)
 
   def Union[T](fields: Field[S] forSome { type S <: T }*): Reader => Option[T] = {
     val builder = immutable.HashTrieMap.Builder[Int, Field[S] forSome { type S <: T }]
@@ -325,7 +327,7 @@ object Protobuf {
 
     override def sizeOf(value: T): Int = {
       if (value != default)
-        sizeOfVarint((tag << 3) | tpe.wireType)                         +
+        sizeOfVarint((tag << 3) | tpe.wireType)                                        +
         (if (tpe.wireType == WireType.Message) sizeOfVarint(tpe.sizeOf(value)) else 0) +
         tpe.sizeOf(value)
       else 0
@@ -344,12 +346,12 @@ object Protobuf {
     override def toString: String = "Protobuf"+"."+"Optional"+"("+ tag +","+ default +")"+"("+ tpe +")"
   }
 
-  private final class Unknown(override val key: Long) extends Field[Unit] {
+  private final class Unknown[T](override val key: Long, val default: T)(implicit override val tpe: Protobuf[T]) extends Field[T] {
+    def this(tag: Int, wireType: Int, default: T)(implicit tpe: Protobuf[T]) = this((tag.toLong << 3) | (wireType & 0x7).toLong, default)(tpe)
+
     override def tag: Int = (key >>> 3).toInt
 
-    override def tpe: Protobuf[Unit] = Unit
-
-    override def readValue(data: Reader): Unit = {
+    override def readValue(data: Reader): T = {
       (key.toInt & 0x7) match {
         case 0 => readVarint(data)
         case 1 => data.readLong()
@@ -357,14 +359,14 @@ object Protobuf {
         case 5 => data.readInt()
         case _ => throw new ProtobufException("unknown wire type: " + wireType)
       }
-      ()
+      default
     }
 
     override def writeKey(data: Writer): Unit = {
       writeVarint(data, key)
     }
 
-    override def writeValue(data: Writer, value: Unit): Unit = {
+    override def writeValue(data: Writer, value: T): Unit = {
       (key.toInt & 0x7) match {
         case 0 => writeVarint(data, 0L)
         case 1 => data.writeLong(0L)
@@ -374,19 +376,19 @@ object Protobuf {
       }
     }
 
-    override def read(data: Reader): Unit = {
+    override def read(data: Reader): T = {
       val key = readVarint(data)
       if ((key >>> 3).toInt != (this.key >>> 3).toInt) throw new ProtobufException(s"expected tag $tag, but found tag ${(key >>> 3).toInt}")
       if ((key.toInt & 0x7) != (this.key.toInt & 0x7)) throw new ProtobufException(s"expected wire type $wireType, but found wire type ${key.toInt & 0x7}")
       readValue(data)
     }
 
-    override def write(data: Writer, value: Unit): Unit = {
+    override def write(data: Writer, value: T): Unit = {
       writeKey(data)
       writeValue(data, value)
     }
 
-    override def sizeOf(value: Unit): Int = (key & 0x7) match {
+    override def sizeOf(value: T): Int = (key & 0x7) match {
       case 0 => 1
       case 1 => 8
       case 2 => 1
@@ -395,16 +397,24 @@ object Protobuf {
     }
 
     override def equals(other: Any): Boolean = other match {
-      case that: Unknown => key == that.key
+      case that: Unknown[_] => key == that.key && default.equals(that.default) && tpe.equals(that.tpe)
       case _ => false
     }
 
     override def hashCode: Int = {
       import MurmurHash3._
-      mash(mix(seed[Unknown], key.##))
+      mash(mix(mix(mix(seed[Unknown[_]], key.##), default.hashCode), tpe.hashCode))
     }
 
-    override def toString: String = "Protobuf"+"."+"Unknown"+"("+"tag"+" = "+ (key >>> 3).toInt +", "+"wireType"+" = "+ (key.toInt & 0x7) +")"
+    override def toString: String = {
+      val s = UString.Builder
+      s.append("Protobuf"); s.append('.'); s.append("Unknown"); s.append('(')
+      s.append("tag");      s.append(" = "); s.show((key >>> 3).toInt); s.append(", ")
+      s.append("wireType"); s.append(" = "); s.show(key.toInt & 0x7);   s.append(", ")
+      s.append("default");  s.append(" = "); s.show(default);
+      s.append(')')
+      s.state.toString
+    }
   }
 
   private final class Union[T](fields: immutable.HashTrieMap[Int, Field[S] forSome { type S <: T }]) extends AbstractFunction1[Reader, Option[T]] {
